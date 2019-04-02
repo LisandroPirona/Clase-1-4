1b877c477b788f0409b10160555b9f6805267254
Alumno5 <Alumno5@5Pentium_6950>
2019-04-01 22:23:58 -0300

    Ejercicios 1-4

---
+++ Ejercicio/src/PedirDatos/NumPares.java
@@ -0,0 +1,25 @@
+package PedirDatos;
+
+public class NumPares {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Imprimir los numeros del 1 al 20 por consola solo los pares
+		
+		/*int i=1;
+		while(i<=5) {
+		System.out.println(i);
+		i++;*/	
+		
+		int i=1;
+		while (i>=20) {
+			
+			if (i%2==0) {
+				System.out.println(i);
+			}
+			System.out.println(i);
+		}
+	}
+
+}
